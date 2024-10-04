236f8427714fd3de30b41f4ba3fbb743e8e701b1
Revanth-Pershad <revanthpershad@gmail.com>
2024-10-04 21:41:54 +0530

    Updated Cors Policy

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 const socketController = require('./controllers/socketController');
 
 const roomRoutes = require('./routes/route.js');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(cors());
 
 app.use('/', roomRoutes);
 
